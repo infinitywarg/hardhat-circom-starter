ab2b4535fb47537a07f411611d07bd8c7e0f90db
infinitywarg <ganesh.k.anantwar@gmail.com>
2023-01-24 19:19:06 +0530

    verifier deployment script

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,23 +1,24 @@
 import { ethers } from "hardhat";
+import { readdirSync } from "fs";
+import { resolve } from "path";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+	const [deployer] = await ethers.getSigners();
+	let Verifier;
+	let verifier;
+	let verifierList: string[] = readdirSync(resolve(__dirname, `../contracts/verifiers`));
+	verifierList = verifierList.map((v) => {
+		return v.split(".")[0];
+	});
+	verifierList.forEach(async (v) => {
+		Verifier = await ethers.getContractFactory(v, deployer);
+		verifier = await Verifier.deploy();
+		await verifier.deployed();
+		console.log(`${v} deployed to ${verifier.address}`);
+	});
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
