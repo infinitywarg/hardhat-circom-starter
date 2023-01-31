5b65fdf256ff035713d5dd75cad4a487cf4b4f19
infinitywarg <ganesh.k.anantwar@gmail.com>
2023-01-31 18:05:24 +0530

    upgrade to v2.1.3 and add support for MacOS

--- bash/0_install_circom.sh
+++ bash/0_install_circom.sh
@@ -2,12 +2,18 @@
 
 CHECK=$(which circom)
 
-if [ -z $CHECK ]
-then
-    echo "circom is not installed, installing v2.1.2"
-    mv ../releases/2.1.2/circom /usr/local/bin
+if [ -z $CHECK ]; then
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "circom is not installed, installing v2.1.3 on Linux"
+        sudo mv ./releases/2.1.3/circom-linux-amd64 /usr/local/bin/circom
+        circom --help
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "circom is not installed, installing v2.1.3 on MacOS"
+        sudo mv ./releases/2.1.3/circom-macos-amd64 /usr/local/bin/circom
+        circom --help
+    fi
 else
     echo "circom is already installed, exiting"
+    circom --help
 fi
 
-circom --help
